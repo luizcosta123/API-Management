787d34088f4e6bbd12846d3f93218e0599d3ce64
Luiz Afonso <luizcosta12322@yahoo.com>
2023-08-13 02:26:09 -0300

    Create PessoaNomePeriodoHorasDto

---
+++ management/src/main/java/com/api/management/dto/PessoaNomePeriodoHorasDto.java
@@ -0,0 +1,22 @@
+package com.api.management.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+public class PessoaNomePeriodoHorasDto {
+
+    private Long id;
+
+    private String nome;
+
+    private LocalDate dataInicio;
+
+    private LocalDate dataFim;
+
+    private int horasGastas;
+
+}
