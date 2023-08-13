230937f47459e07efe11d57da252c13d8ba27994
Luiz Afonso <luizcosta12322@yahoo.com>
2023-08-13 02:16:34 -0300

    Add new exceptions

--- management/src/main/java/com/api/management/exception/ControllerException.java
+++ management/src/main/java/com/api/management/exception/ControllerException.java
@@ -1,31 +1,44 @@
 package com.api.management.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.lang.IllegalArgumentException;
 
 @RestControllerAdvice
 public class ControllerException {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public List<ErrorHandle> handle(MethodArgumentNotValidException exception) {
-
+    public ResponseEntity<List<StandardError>> MethodArgumentNotValidHandlerMethod(MethodArgumentNotValidException exception, HttpServletRequest request) {
         List<FieldError> fieldErrorList = exception.getBindingResult().getFieldErrors();
-
-        List<ErrorHandle> errorHandleList = new ArrayList<>();
+        List<StandardError> errorHandleList = new ArrayList<>();
 
         fieldErrorList.forEach(error -> {
-            errorHandleList.add(new ErrorHandle(error.getField(), error.getDefaultMessage()));
+            errorHandleList.add(new StandardError(LocalDateTime.now(), 400, "Bad Request", error.getDefaultMessage(), request.getRequestURI()));
         });
 
-        return errorHandleList;
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorHandleList);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<StandardError> HttpMessageNotReadableHandlerMethod(HttpMessageNotReadableException exception, HttpServletRequest request) {
+        StandardError se = new StandardError(LocalDateTime.now(), 400, "Bad Request", exception.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(se);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<StandardError> IllegalArgumentExceptionHandlerMethod(IllegalArgumentException exception, HttpServletRequest request) {
+        StandardError se = new StandardError(LocalDateTime.now(), 400, "Bad Request", exception.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(se);
     }
 
 }
