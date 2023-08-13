a7d21a9f81af89680b7c34c0d3ba7ea6ffbd447a
Luiz Afonso <luizcosta12322@yahoo.com>
2023-08-13 02:18:46 -0300

    Create DepartamentoDto

---
+++ management/src/main/java/com/api/management/dto/DepartamentoDto.java
@@ -0,0 +1,14 @@
+package com.api.management.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DepartamentoDto {
+
+    private Long id;
+
+    private String nome;
+
+}
