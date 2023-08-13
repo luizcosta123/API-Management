c465e4496d0d9b7e950381ce41455287d87dda8f
Luiz Afonso <luizcosta12322@yahoo.com>
2023-08-13 02:29:02 -0300

    Add @Min validation

--- management/src/main/java/com/api/management/form/TarefaForm.java
+++ management/src/main/java/com/api/management/form/TarefaForm.java
@@ -1,7 +1,7 @@
 package com.api.management.form;
 
 import com.api.management.enuns.Departamento;
-import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -28,6 +28,7 @@ public class TarefaForm {
     @NotNull(message = "O campo 'duracao' não pode estar vazio!")
     private Integer duracao;
 
+    @Min(value = 1, message = "O ID de pessoaAlocada precisa ser maior que 1!")
     private Long pessoaAlocada;
 
     @NotNull(message = "O campo 'nome' não pode estar vazio!")
